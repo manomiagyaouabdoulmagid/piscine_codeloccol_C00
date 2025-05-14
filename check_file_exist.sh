7e0510b0c766d2de4a7023284b1e18fbf0a98981
manomiagyaouabdoulmagid <cnoniger@gmail.com>
2025-05-14 23:30:02 +0200

    Add files via upload

---
+++ check_file_exist.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Ce script vérifie si un fichier existe
+
+read -p "Entrez le nom du fichier : " filename
+
+if [ -f "$filename" ]; then
+    echo "Le fichier '$filename' existe."
+else
+    echo "Le fichier '$filename' n'existe pas."
+fi
---
+++ list_files.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Ce script affiche la liste des fichiers du répertoire courant
+
+ls -la
